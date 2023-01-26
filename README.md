d1671a01f3de49a783a0b14681c40a91c20d0b02
htaylor <hilerytaylorjr@gmail.com>
2023-01-26 01:16:26 -0800

    bug fix for space path directories

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# backup_script
-01/2023 - version 1.0 - written by Hilery Taylor - https://github.com/htaylor777/backup_script
- - install script to /usr/local/bin 
- - sym link it: ln -s /usr/local/bin/rsync_to_backup.sh backup
- - you should be able to call it anywhere on the system using keyword "backup"
+# backup_script files and directory for MAC OS and Linux
++ 01/2023 - version 1.0 - written by Hilery Taylor - https://github.com/htaylor777/backup_script
+  OS: MAC, Linux
+  install script to /usr/local/bin or your bin PATH for global access
+# ----------------------------------------------------------------------------
+  ***INSTRUCTIONS:
+  Configure your file backup server location(s):
+-  Line 52: @DEFINE your external BACKUPDRIVE(S): BACKUPDRIVE=<pathToYourFileBackupDrive>[1] [2] [etc]
+  
+  ***Configure your directory backup location:
+-  Line 70: @DEFINE your external BACKUPDRIVE: CLIENT=<pathToYourDirectoryBackupDrive>
+-  move this script to your bin location, i.e. /usr/local/bin or your specific bin $PATH
+-  sym link it: ln -s /usr/local/bin/rsync_to_backup.sh backup
+-  you should be able to call it anywhere on the system using keyword "backup"
--- rsync_to_backup.sh
+++ rsync_to_backup.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 #----------------------------------------------------------------------------
 #  01/2023 - version 1.0
-#  Hilery Taylor - https://github.com/htalor777/auto_backup
-#  install script to /usr/local/bin 
+#  Hilery Taylor - https://github.com/htalor777/backup_script
+#  OS: MAC, Linux
+#  install script to /usr/local/bin or your bin PATH for global access
+# ----------------------------------------------------------------------------
+#  ***INSTRUCTIONS:
+#  Configure your file backup server location(s):
+#  Line 52: @DEFINE your external BACKUPDRIVE(S): BACKUPDRIVE=<pathToYourFileBackupDrive>[1] [2] [etc]
+#  
+#  ***Configure your directory backup location:
+#  Line 70: @DEFINE your external BACKUPDRIVE: CLIENT=<pathToYourDirectoryBackupDrive>
+#  move this script to your bin location, i.e. /usr/local/bin or your specific bin PATH
 #  sym link it: ln -s /usr/local/bin/rsync_to_backup.sh backup
 #  you should be able to call it anywhere on the system using keyword "backup"
 #----------------------------------------------------------------------------
 
-export CURRENT=`pwd`
-
-function procbackup() {
+function proc_file_backup() {
 fl=$1
 clbackup=$2
 dir=$3
 
 if [[ ! -z "$3"  && ! -d $clbackup/$dir ]]; then 
 echo "attempting to mkdir backup $fl to $clbackup/$dir"
-mkdir -p $clbackup/$dir
-rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable $fl $clbackup/$dir 
+mkdir -p $clbackup/"$dir"
+rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable "$fl" $clbackup/$dir 
 exit 0
 
-elif [[ ! -z "$3"  && -d $clbackup/$dir ]]; then
+elif [[ ! -z "$3"  && -d $clbackup/"$dir" ]]; then
 echo "$dir exists: attempting to backup $fl to $clbackup/$dir"
-rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable $fl $clbackup/$dir 
+rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable "$fl" $clbackup/$dir 
 exit 0
 
 elif [  -z "$3" ]; then
 echo "attempting to direct volume backup $fl to $clbackup"
-rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable $fl $clbackup
+rsync -az --progress --no-perms --no-group --no-owner --stats --human-readable "$fl" $clbackup
 exit 0
 fi
 }
@@ -48,18 +55,19 @@ echo "...Quitting"
 exit 0
 fi
 
+# @DEFINE your external BACKUPDRIVE(S) below: BACKUPDRIVE=<pathtoyourbackupdrive>
 echo "If there is a backup sub-directory name in the backup $client enter it here: "
 read dirext
 
   if [ $client == 1 ]; then
-     CLIENT="/Volumes/Client_1"
-     procbackup $INFILE $CLIENT $dirext
+     BACKUPDRIVE="/Volumes/Client_1"
+     proc_file_backup "$INFILE" $BACKUPDRIVE $dirext
 
  elif [ $client == 2 ]; then
-     CLIENT="/Volumes/client_2"
-     procbackup $INFILE $CLIENT $dirext
+     BACKUPDRIVE="/Volumes/client_2"
+     proc_file_backup "$INFILE" $BACKUPDRIVE $dirext
 
- else echo "invalid choice, choose 1 or 2"    
+ else echo "invalid choice, choose 1 or 2 for backup drives"    
 fi
 }
 
@@ -75,7 +83,7 @@ mkdir -p "${CLIENT}/$ArcDate"
 fi
 
 echo "Attempting to backup your directory: $INDIR to ${CLIENT}/$ArcDate"
-rsync -vrltD --progress --no-perms --no-group --no-owner --stats --human-readable ${INDIR} ${CLIENT}/$ArcDate --exclude=".*/" 
+rsync -vrltD --progress --no-perms --no-group --no-owner --stats --human-readable "$INDIR" $CLIENT/$ArcDate --exclude=".*/" 
 ##rsync -vrltD --progress --no-perms --no-group --no-owner --stats --human-readable ${INDIR} ${CLIENT}/$ArcDate --exclude=".*/" |  pv -lep -s 42 >/dev/null
 exit 0
 }
@@ -85,10 +93,10 @@ function pathfile_backup() {
 echo " What is your File Name to backup? "
 read thispathfile
 
-if [ -e "${CURRENT}/$thispathfile" ]; then 
-file_bkup_init ${CURRENT}/$thispathfile
+if [ -e "$(pwd)"/$thispathfile ]; then 
+file_bkup_init "$(pwd)"/$thispathfile
 else
-echo "ERROR: your file: ${CURRENT}/$thispathfile does not exist"
+echo "ERROR: your file: $(pwd)/$thispathfile does not exist"
 echo "ABORTING BACKUP"
 exit 1
 fi
@@ -100,10 +108,9 @@ function directory_backup() {
 echo " What is your Directory Name that you want to backup? "
 read thisdirectory
 
-if [ -e "${CURRENT}/$thisdirectory" ]; then 
-dir_bkup_init ${CURRENT}/$thisdirectory
-else
-echo "ERROR: your directory: ${CURRENT}/$thisdirectory does not exist"
+if [ -e "$(pwd)"/$thisdirectory ]; then
+dir_bkup_init "$(pwd)"/$thisdirectory
+echo "ERROR: your directory: "$(pwd)"/$thisdirectory does not exist"
 echo "ABORTING BACKUP"
 exit 1
 fi
@@ -112,13 +119,15 @@ fi
 
 function init() {
 
-echo "|||||||||||||||||||||||||||||||||||||||||||||||||||||"
+echo "||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+echo "                                                      "
+echo "	BACKUP DIRECTORY OR FILE TO BACKUP DRIVES:         "
+echo "______________________________________________________"
+echo "            Select number of backup type: "
+echo "             Directory Backup->[0]"
+echo "                  File Backup->[1]"
+echo "                     or Quit->[-1]"
 echo "____________________________________________________"
-echo "	RSYNC FILE TO BACKUP DRIVE:"
-echo "____________________________________________________"
-echo " Select number of backup type: "
-echo " Directory Backup->[0] or File Backup->[1] Quit->[-1]"
-
 while read bktype
 do
 if [ $bktype -eq -1 ]; then
